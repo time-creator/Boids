2719728c42917e02d8c898bb3b22345c125f9dd5
TimTen10 <Tim.Eckle94@gmail.com>
2020-08-25 18:02:41 +0200

    pygame base loop

--- main.py
+++ main.py
@@ -2,4 +2,38 @@ import pygame
 
 print(pygame.__version__)
 
-print('Testing on second machine')
+
+def main():
+    # Initialise the screen
+    pygame.init()
+    screen = pygame.display.set_mode((150, 50))
+    pygame.display.set_caption("Basic Pygame program")
+
+    # Fill background
+    background = pygame.Surface(screen.get_size())
+    background = background.convert()
+    background.fill((250, 250, 250))
+
+    # Display some text
+    font = pygame.font.Font(None, 36)
+    text = font.render('Hello there', 1, (10, 10, 10))
+    textpos = text.get_rect()
+    textpos.centerx = background.get_rect().centerx
+    background.blit(text, textpos)
+
+    # Blit everything to the screen
+    screen.blit(background, (0, 0))
+    pygame.display.flip()
+
+    # Event (Main) loop
+    while 1:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.blit(background, (0, 0))
+        pygame.display.flip()
+
+
+if __name__ == '__main__':
+    main()
