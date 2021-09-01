bdf81bc580002d38c89eda843e97b4a7c59c7ede
TimTen10 <Tim.Eckle94@gmail.com>
2021-09-01 15:01:31 +0200

    Bound checking is done by the main loop, not the Boid class.

--- main.py
+++ main.py
@@ -3,16 +3,25 @@ import random
 from boid import Boid
 
 
+WIDTH = 600
+HEIGHT = 600
+FPSCLOCK = pygame.time.Clock()
+FPS = 30
+
+
 def main():
     # Initialise the screen
     pygame.init()
-    screen = pygame.display.set_mode((1200, 750))
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
     screen.fill((255, 255, 255))
     print(screen.get_size())
     pygame.display.set_caption("Basic Pygame program")
 
     # Create some Boids
-    flock = [Boid(random.randint(50, 1150), random.randint(50, 700), [3, 0]) for i in range(10)]
+    flock = [Boid(
+        random.randint(10, WIDTH - 10), random.randint(10, HEIGHT - 10),
+        [random.randint(-5, 5), random.randint(-5, 5)]
+    ) for i in range(20)]
     for bird in flock:
         pygame.draw.circle(screen, pygame.Color('red'), (bird.x, bird.y), 3)
 
@@ -35,8 +44,18 @@ def main():
             # equals "show"
             pygame.draw.circle(screen, pygame.Color('red'), (bird.x, bird.y), 3)
             bird.update()
+            if bird.x < 0:
+                bird.x = WIDTH
+            elif bird.x > WIDTH:
+                bird.x = 0
+            if bird.y < 0:
+                bird.y = HEIGHT
+            elif bird.y > HEIGHT:
+                bird.y = 0
 
         pygame.display.flip()
+
+        FPSCLOCK.tick(FPS)
 
 
 if __name__ == '__main__':
