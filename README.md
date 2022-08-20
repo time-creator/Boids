d779c046512113b0794463560e4c3b21f3e290ce
TimTen10 <Tim.Eckle94@gmail.com>
2022-08-20 17:58:19 +0200

    Updated utility functions.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Boids
 A Python project to visualise Boids.
+Inspiration can be found [here](https://vergenet.net/~conrad/boids/pseudocode.html).
--- util.py
+++ util.py
@@ -1,23 +1,36 @@
 import math
 
 
-def vector_add(v: list[float], w: list[float]) -> list[float]:
+Vector = list[float]
+
+
+def vector_add(v: Vector, w: Vector) -> Vector:
+    """Adds Vector v and Vector w."""
     return [v_i + w_i for v_i, w_i in zip(v, w)]
 
 
-def magnitude(v: list[float]) -> float:
+def vector_sub(v: Vector, w: Vector) -> Vector:
+    """Subtracts Vector w from Vector v."""
+    return [v_i - w_i for v_i, w_i in zip(v, w)]
+
+
+def scalar_product(v: Vector, k: float) -> Vector:
+    return [v_i * k for v_i in v]
+
+
+def scalar_division(v: Vector, k: float) -> Vector:
+    return [v_i / k for v_i in v]
+
+
+def magnitude(v: Vector) -> float:
     return math.sqrt(sum(v_i ** 2 for v_i in v))
 
 
-def normalize(v: list[float]) -> list[float]:
+def normalize(v: Vector) -> Vector:
     magnitude_v = magnitude(v)
     return [v_i / magnitude_v for v_i in v]
 
 
-def scalar_product(v: list[float], k: float) -> list[float]:
-    return [v_i * k for v_i in v]
-
-
-def distance(v: list[float], w: list[float]) -> float:
+def distance(v: Vector, w: Vector) -> float:
     difference_vector = [v_i - w_i for v_i, w_i in zip(v, w)]
     return math.sqrt(sum(x_i ** 2 for x_i in difference_vector))
