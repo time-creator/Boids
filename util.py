dc438271bcbbd1460df6c17657288d8a3d3a6f07
Tim Eckle <Tim.Eckle94@gmail.com>
2022-08-26 14:18:17 +0200

    Added tests.

--- util.py
+++ util.py
@@ -36,3 +36,19 @@ def normalize(v: Vector) -> Vector:
 def distance(v: Vector, w: Vector) -> float:
     difference_vector = [v_i - w_i for v_i, w_i in zip(v, w)]
     return math.sqrt(sum(x_i ** 2 for x_i in difference_vector))
+
+
+if __name__ == '__main__':
+    # Test vector_add
+    assert vector_add([4, 3, 2, 1], [6, 7, 8, 9]) == [10, 10, 10, 10]
+    assert vector_add([-1, 1, -1, 1], [0, 0, 0, 0]) == [-1, 1, -1, 1]
+    assert vector_add([35, 8, 123, 0], [1, 74, 78, 22]) == [36, 82, 201, 22]
+
+    # Test vector_sub
+    assert vector_sub([10, 10, 10, 10], [6, 7, 8, 9]) == [4, 3, 2, 1]
+    assert vector_sub([-1, 1, -1, 1], [0, 0, 0, 0]) == [-1, 1, -1, 1]
+    assert vector_sub([36, 82, 201, 22], [1, 74, 78, 22]) == [35, 8, 123, 0]
+
+    # Test scalar_product
+
+    # Test scalar_division
